ae62368d9f47e71298338477f3a3c8cf290c692c
mdhabibur <mdhrahman532@gmail.com>
2022-01-05 10:11:24 +0600

    First commit done to this repository

---
+++ app/src/main/java/com/mmali/recognizeclassandmethods/MainActivity.java
@@ -0,0 +1,22 @@
+package com.mmali.recognizeclassandmethods;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //class name always starts with capital letter
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //method name starts with small letter by convention
+
+        //camelCase method is always used in java to write variables, class, method name
+        //reserved keys are marked in blue color in android studio and you can't use them to write your variables
+
+
+
+    }
+}
