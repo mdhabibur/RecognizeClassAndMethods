2c2e489b8744f981c58f7e2992c2aaed79c714f6
mdhabibur <mdhrahman532@gmail.com>
2022-01-05 10:27:18 +0600

    Second commit done to this repository

--- app/src/main/java/com/mmali/recognizeclassandmethods/MainActivity.java
+++ app/src/main/java/com/mmali/recognizeclassandmethods/MainActivity.java
@@ -3,8 +3,13 @@ package com.mmali.recognizeclassandmethods;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    //github personal access token: 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
+
+
     //class name always starts with capital letter
 
     @Override
@@ -16,7 +21,14 @@ public class MainActivity extends AppCompatActivity {
         //camelCase method is always used in java to write variables, class, method name
         //reserved keys are marked in blue color in android studio and you can't use them to write your variables
 
+        showSecondCommit();
+
 
 
     }
+
+    public void showSecondCommit(){
+        Toast.makeText(MainActivity.this, "This is second commit to this repository", Toast.LENGTH_SHORT).show();
+    }
+
 }
